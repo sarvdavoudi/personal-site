4df7255dfa7396bdd255ec85a5888b5eb6b1e352
sarvdavoudi <sarvdavoudi3000@yahoo.com>
2023-05-21 12:00:40 +0330

    feat:scroll-to-up added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <header>
       <ul>
        
-        <a href="#"><li>Home</li></a>
+        <a href="#home"><li id="home">Home</li></a>
         <a href="#about-me"><li>About</li></a>
         <a href="#resume"><li>Resume</li></a>
         <a href="#contact"><li>Contact</li></a>
